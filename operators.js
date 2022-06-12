159e06879c372d79767841ba55008f48f8e1b8ef
akumaru <isakulovDev@gmail.com>
2022-06-12 10:57:45 +0500

    operators.js

---
+++ operators.js
@@ -0,0 +1,34 @@
+let a = 10
+let b = 20
+
+// Addition operator
+let c = a + b
+console.log(c)
+
+// Subtraction operator
+let d = a - b
+console.log(d)
+
+// Multiplication operator
+let e = a * b
+console.log(e)
+
+// Division operator
+let f = a / b
+console.log(f)
+
+// Exponentation operator
+let g = a ** b
+console.log(g)
+
+// Modulus operator
+let h = a % b
+console.log(h)
+
+// Increment operator
+let i = a++
+console.log(i)
+
+// Decrement operator
+let j = a--
+console.log(j)
