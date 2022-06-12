c9c924534f5bc9ef92b1c2a613c2300dbc62e3db
akumaru <isakulovDev@gmail.com>
2022-06-12 10:57:28 +0500

    functions.js

---
+++ functions.js
@@ -0,0 +1,21 @@
+// Defining Function
+
+// First way
+
+function add(a, b) {
+    return a + b
+}
+
+// Second way
+
+add = (a, b) => a + b
+
+// Third way
+
+add = (a, b) => {
+    return a + b
+}
+
+
+let result = add(10, 20)
+console.log(result) 
